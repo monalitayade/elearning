13f0c85d37e543395fbd0a470b535b1dd5f20ef8
Monali_Tayade <c-monali.tayade@timesgroup.com>
2025-03-08 14:58:21 +0530

    subject page changes

--- js/common-js.js
+++ js/common-js.js
@@ -1,25 +1,80 @@
 jQuery(document).ready(function () {
-  //for web view
-  jQuery(".updates-links").on("click", function () {
-    var active_pannel = jQuery(this).attr("data-href");
-    var active_pannel = active_pannel.replace(/^#/, ""); // Remove # from start of string
-    console.log("active-pannel", active_pannel);
-    jQuery(".updates-li .updates-links").parent().removeClass("active");
-    jQuery(this).parent().addClass("active");
-    jQuery(".updates-links.mob-link ").removeClass("active");
-    jQuery(this).addClass("active");
-    jQuery(".pannel-block").removeClass("active");
-    jQuery(`#${active_pannel}`).addClass("active").show();
+  //collapse navigation for web
+  // jQuery(".hamburger-icon").on("click", function () {
+  //   if (jQuery(".chapter-topics-layout").hasClass("open")) {
+  //     console.log("collapse");
+
+  //     jQuery(".right-side-wrapper").addClass("full-width");
+  //     jQuery(".chapter-topics-layout").removeClass("open");
+  //   } else {
+  //     console.log("open");
+  //     jQuery(".chapter-topics-layout").addClass("open");
+  //     jQuery(".right-side-wrapper").removeClass("full-width");
+  //   }
+  // });
+
+  jQuery(".topic-link").click(function (e) {
+    e.preventDefault();
+    // jQuery(this).next().slideToggle();
+    // jQuery(this).toggleClass("active");
+    console.log("hii");
+
+    if (jQuery(this).hasClass("active") === false) {
+      jQuery(".topic-link").removeClass("active");
+      jQuery(this).addClass("active");
+    }
+  });
+  jQuery(".mobile-topic-close").click(function () {
+    jQuery(".chapter-topics-layout").animate({ left: "-100%" }, 500);
   });
 
-  //for mobile and tab view
-  // jQuery(".updates-links").on("click", function () {
-  //   var active_pannel = jQuery(this).attr("data-href");
-  //   var active_pannel = active_pannel.replace(/^#/, ""); // Remove # from start of string
-  //   console.log("active-pannel", active_pannel);
-  //   // jQuery(".updates-li .updates-links").parent().removeClass("active");
-  //   // jQuery(this).parent().addClass("active");
-  //   // jQuery(".pannel-block").removeClass("active");
-  //   // jQuery(`#${active_pannel}`).addClass("active").show();
-  // });
+  //search popup
+  jQuery(".search-btn").on("click", function () {
+    jQuery(".search-popup").addClass("active");
+  });
+  jQuery(".popup-overlay").on("click", function () {
+    jQuery(".search-popup").removeClass("active");
+  });
+
+  $(".hamburger-icon").click(function () {
+    // Assuming you have a toggle button/element with class toggle-nav
+    $(".chapter-topics-layout").toggleClass("open");
+    $(".right-side-wrapper").toggleClass("full-width");
+  });
+
+  if (window.innerWidth <= 1112) {
+    jQuery(".mobile-close").on("click", function () {
+      jQuery(".chapter-topics-layout").removeClass("open");
+    });
+  }
+
+  //accordian jquery
+  $(".pannel-section").click(function (e) {
+    var $pannelSection = $(this);
+    var $link = $pannelSection.find(".updates-links"); // Find the associated link
+    var $target = $($link.data("href")); // Find the associated content
+
+    // Prevent the default anchor behavior if the click was on the link itself.
+    if (
+      $(e.target).hasClass("updates-links") ||
+      $(e.target).closest(".updates-links").length
+    ) {
+      e.preventDefault();
+    }
+
+    if ($pannelSection.hasClass("active")) {
+      $target.slideUp(500, function () {
+        $pannelSection.removeClass("active");
+      });
+    } else {
+      $target.slideDown(500, function () {
+        $pannelSection.addClass("active");
+      });
+    }
+  });
+
+  //profile popup icon
+  jQuery(".profile-plot").on("click", function () {
+    jQuery(".profile-popup").toggleClass("show-profile-items");
+  });
 });
